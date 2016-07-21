657dacfe47d3aae3330ea5ebdf67419363160f43
StreatCodes <matt.r.streatfield@gmail.com>
2016-07-21 16:19:37 +1000

    Added support for unauthenticated email, adjusted usaged text, updated readme, added aditional information to emails.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # talkus-listener
 An http webhook listener for talkus.io chat transcripts.
 Accepts JSON posted by talkus and emails the transcript to your chosen email address.
+Support both authenticated and un-authenticated email.
+
+Build with `go build`
+
+Example usage:
+`./talkus-listener --emailRecipient recieving@example.com --emailSender sending@example.com --emailServer mail.example.com`
 
 ### TODO
 
-* Build & installation instructions
-* Usage instructions
 * Init scripts
--- main.go
+++ main.go
@@ -17,16 +17,16 @@ var emailRecipient string
 //initialize application flags
 func init() {
 	flag.StringVar(&emailServer, "emailServer", "", "SMTP server used to send mail from, requires associated username and password")
-	flag.StringVar(&emailSender, "emailSender", "", "Email address used to send tickets.")
-	flag.StringVar(&emailPassword, "emailPassword", "", "Password associated with the sending email address.")
 	flag.StringVar(&emailRecipient, "emailRecipient", "", "Email address used to recieve the transcript")
+	flag.StringVar(&emailSender, "emailSender", "", "Email address used to send tickets.")
+	flag.StringVar(&emailPassword, "emailPassword", "", "(OPTIONAL) Password associated with the sending email address. If none supplied there will be no authentication")
 	flag.Parse()
 }
 
 //Main runs http server if appropriate flags specified
 func main() {
-	if emailServer == "" || emailSender == "" || emailPassword == "" || emailRecipient == "" {
-		fmt.Println("All flags are required. Run with --help for info.")
+	if emailServer == "" || emailSender == "" || emailRecipient == "" {
+		fmt.Println("emailServer, emailSender and emailRecipient required. Run with --help for info.")
 	} else {
 		http.HandleFunc("/transcript", handler)
 		http.ListenAndServe(":8080", nil)
@@ -35,12 +35,21 @@ func main() {
 
 //Stores message data, includes requried JSON tags
 type transcript struct {
-	Event       string `json:"event"`
-	CreatedAt   string `json:"createdAt"`
-	ChannelName string `json:"channelName"`
-	VisitorName string `json:"visitorName"`
-	VisitorID   string `json:"visitorId"`
-	Message     string `json:"message"`
+	Event       string   `json:"event"`
+	CreatedAt   string   `json:"createdAt"`
+	ChannelName string   `json:"channelName"`
+	VisitorName string   `json:"visitorName"`
+	VisitorID   string   `json:"visitorId"`
+	Identity    identity `json:"identity"`
+	Message     string   `json:"message"`
+}
+
+type identity struct {
+	UserAgent string `json:"userAgent"`
+	Location  string `json:"location"`
+	Title     string `json:"title"`
+	IP        string `json:"ip"`
+	Languages string `json:"languages"`
 }
 
 //Gets transcript data in a readable format
@@ -49,12 +58,14 @@ func (t transcript) toString() string {
 	if err != nil {
 		fmt.Printf("Error parsing time: %s\n", err)
 	}
-	currentTime := time.Now()
 
-	return fmt.Sprintf("Visitor name: %s\nCreated on: %s\nArchived on: %s\n\n%s",
+	return fmt.Sprintf("Visitor name: %s\nChannel name: %s\nVisitor ID: %s\nArchived on: %s\nUser agent: %s\nUser IP: %s\n\n%s",
 		t.VisitorName,
+		t.ChannelName,
+		t.VisitorID,
 		createdTime.Local().Format(time.Stamp),
-		currentTime.Format(time.Stamp),
+		t.Identity.UserAgent,
+		t.Identity.IP,
 		t.Message)
 }
 
@@ -70,25 +81,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	//Loop through objects, if message is present email the transcript
 	for _, i := range t {
 		if i.Message != "" {
-			sendEmail(emailServer, emailSender, emailPassword, emailRecipient, i.toString())
+			sendEmail(emailServer, emailSender, emailPassword, emailRecipient, i)
 			fmt.Println(i.toString())
 		}
 	}
 }
 
 //Send message to email
-func sendEmail(emailServer string, emailSender string, emailPassword string, emailRecipient string, message string) {
+func sendEmail(emailServer string, emailSender string, emailPassword string, emailRecipient string, trans transcript) {
 	// Set up authentication information.
-	auth := smtp.PlainAuth("", emailSender, emailPassword, emailServer)
+	var auth smtp.Auth
+	if emailPassword != "" {
+		auth = smtp.PlainAuth("", emailSender, emailPassword, emailServer)
+	}
 
-	// Connect to the server, authenticate, set the sender and recipient,
-	// and send the email all in one step.
+	// Set up mail headers and content
 	to := []string{emailRecipient}
 	msg := []byte("To: " + emailRecipient + "\r\n" +
-		"Subject: SMTP test\r\n" +
+		"Subject: Live support with " + trans.VisitorName + "\r\n" +
 		"\r\n" +
-		message + "\r\n")
+		trans.toString() + "\r\n")
+
+	//Send mail, if password supplied use auth.
 	err := smtp.SendMail(emailServer+":25", auth, emailSender, to, msg)
+
 	if err != nil {
 		fmt.Printf("Failed to send mail: %s\n", err)
 	} else {
